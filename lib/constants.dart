4d7bfc81fbb77bd03f3f784f0d54fba106fd9df8
nowxel <58902795+nowxel@users.noreply.github.com>
2020-12-01 18:09:32 +0200

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+class Constants{
+  static const String Name = 'Name';
+  static const String Priority = 'Priority';
+  static const String Date = 'Date';
+
+  static const List<String> chioses = <String> [
+    Name,
+    Priority,
+    Date
+  ];
+}
